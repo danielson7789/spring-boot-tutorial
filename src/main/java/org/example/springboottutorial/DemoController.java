c26524f064f9332bcb8531a139c5e347da061816
danielson7789 <dklenn7789@googlemail.com>
2025-01-10 16:26:46 +0100

    Put Mapping updatet

--- src/main/java/org/example/springboottutorial/DemoController.java
+++ src/main/java/org/example/springboottutorial/DemoController.java
@@ -33,7 +33,7 @@ public class DemoController {
         customerRepository.deleteById(id);
     }
 
-    @GetMapping("/update")
+    @PutMapping("/update")
     public String updateCustomer(@RequestParam String first, @RequestParam String last , @RequestParam Integer id) {
         Customer customer = customerRepository.findCustomerById(id);
 
@@ -41,7 +41,8 @@ public class DemoController {
             customer.setFirstName(first);
             customer.setLastName(last);
             customerRepository.save(customer);
+            return "Customer updated!" + customer.getFirstName() + " " + customer.getLastName() + " " + customer.getId();
         }
-        return "Customer updated!" + customer.getFirstName() + " " + customer.getLastName() + " " + customer.getId();
+        return "Customer not found!";
     }
 }
